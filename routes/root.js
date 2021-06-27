0bf229033fe033538cc6517636a4a27fa22339a9
Dave Mount <davemount@gmail.com>
2021-06-27 11:35:25 -0700

    Progress towards enabling https.

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,16 @@
-'use strict'
+"use strict";
+const path = require("path");
+const fs = require("fs");
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    return { root: true };
+  });
+};
+
+module.exports.options = {
+  https: {
+    key: fs.readFileSync(path.join(__dirname, "../../local/server.key")),
+    cert: fs.readFileSync(path.join(__dirname, "../../local/server.cert")),
+  },
+};
