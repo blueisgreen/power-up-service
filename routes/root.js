a9a3d3572969028875c056497d61c1a6d2a45c15
Dave Mount <davemount@gmail.com>
2021-06-28 21:46:49 -0700

    Clean up

--- routes/root.js
+++ routes/root.js
@@ -1,16 +1,7 @@
 "use strict";
-// const path = require("path");
-// const fs = require("fs");
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
     return { root: true };
   });
 };
-
-// module.exports.options = {
-//   https: {
-//     key: fs.readFileSync(path.join(__dirname, "../../local/server.key")),
-//     cert: fs.readFileSync(path.join(__dirname, "../../local/server.cert")),
-//   },
-// };
