4bb2bbaf85888b68fc4e7044250e20f59f857a24
Dave Mount <davemount@gmail.com>
2021-07-24 16:12:11 -0700

    Move route login logic under routes.

--- plugins/3-4-oauth2.js
+++ plugins/3-4-oauth2.js
@@ -23,29 +23,5 @@ module.exports = fp(async function (fastify, opts) {
     callbackUri: `http://localhost:${port}/login/github/callback`,
   })
 
-  fastify.get('/login/github/callback', async function (request, reply) {
-    const token =
-      await this.githubOAuth2.getAccessTokenFromAuthorizationCodeFlow(request)
-    fastify.log.info(token.access_token)
-
-    // try to get user info from GitHub
-    const userInfo = await fastify.axios.request({
-      url: 'https://api.github.com/user',
-      method: 'get',
-      headers: {
-        'Authorization': `token ${token.access_token}`
-      }
-    })
-    fastify.log.info('found user info')
-    fastify.log.info(JSON.stringify(userInfo.data))
-
-    // to refresh token at some point, use
-    // const newToken = await this.getNewAccessTokenUsingRefreshToken(token.refresh_token)
-
-    reply.header('x-access-blargy', token.access_token)
-    reply.send({ 'message': 'Hello, Blargy' })
-    // reply.redirect('http://localhost:8080/')
-
-    // use access_token to create jwt
-  })
+  // set up more OAuth providers below
 })
