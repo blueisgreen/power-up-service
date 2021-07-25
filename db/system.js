a11794f2ae84aa81bc89215cb19442037e6ba08f
Dave Mount <davemount@gmail.com>
2021-07-25 15:03:33 -0700

    Loads system codes

--- db/system.js
+++ db/system.js
@@ -10,13 +10,69 @@ const createSystemCodesTable = async (fastify) => {
     table.integer('parent_id') // establish heirarchy of codes
   })
   log.info(`created ${systemCodesTableName}`)
-  await knex(systemCodesTableName).insert(
+  const categories = await knex(systemCodesTableName).insert(
     [
       { public_id: 'role', display_name: 'Role' },
       { public_id: 'accountStatus', display_name: 'Account Status' },
       { public_id: 'socialPlatform', display_name: 'Social Platform' },
+    ], ['id', 'public_id']
+  )
+  const roleCat = categories.find(cat => cat.public_id === 'role')
+  await knex(systemCodesTableName).insert(
+    [
+      {public_id: 'admin', display_name: 'System Administrator', parent_id: roleCat.id },
+      {public_id: 'editorInChief', display_name: 'Editor in Chief', parent_id: roleCat.id },
+      {public_id: 'editor', display_name: 'Editor', parent_id: roleCat.id },
+      {public_id: 'member', display_name: 'Member', parent_id: roleCat.id },
+      {public_id: 'guest', display_name: 'Guest', parent_id: roleCat.id },
     ]
   )
+  const accountStatusCat = categories.find(cat => cat.public_id === 'accountStatus')
+  await knex(systemCodesTableName).insert([
+    {
+      public_id: 'active',
+      display_name: 'Active',
+      parent_id: accountStatusCat.id,
+    },
+    {
+      public_id: 'suspended',
+      display_name: 'Suspended',
+      parent_id: accountStatusCat.id,
+    },
+    {
+      public_id: 'canceled',
+      display_name: 'Canceled',
+      parent_id: accountStatusCat.id,
+    },
+    {
+      public_id: 'archived',
+      display_name: 'Archived',
+      parent_id: accountStatusCat.id,
+    },
+  ])
+  const socialPlatformCat = categories.find(cat => cat.public_id === 'socialPlatform')
+  await knex(systemCodesTableName).insert([
+    {
+      public_id: 'github',
+      display_name: 'GitHub',
+      parent_id: socialPlatformCat.id,
+    },
+    {
+      public_id: 'google',
+      display_name: 'Google',
+      parent_id: socialPlatformCat.id,
+    },
+    {
+      public_id: 'linkedIn',
+      display_name: 'Linked In',
+      parent_id: socialPlatformCat.id,
+    },
+    {
+      public_id: 'twitter',
+      display_name: 'Twitter',
+      parent_id: socialPlatformCat.id,
+    },
+  ])
   log.info(`loaded codes into ${systemCodesTableName}`)
 }
 
