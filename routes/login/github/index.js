ff9d0085a7dfff842b8c9aa9ed31c558efb9b7cc
Dave Mount <davemount@gmail.com>
2021-07-25 18:38:20 -0700

    Sketch things in - save progress

--- routes/login/github/index.js
+++ routes/login/github/index.js
@@ -17,16 +17,24 @@ module.exports = async function (fastify, opts) {
     fastify.log.info('found user info')
     fastify.log.info(JSON.stringify(userInfo.data))
 
+    // check database for user; add if missing and store profile info, including access_token
+
+    const { knex } = fastify
+    // const socialUserId = knex('social_profiles')
+    //   .select('user_id')
+    //   .join('system_codes', 'social_profiles.') // system_codes.public_id = 'github' and system_codes.id = social_profiles.social_platform_id
+    //   .where({ social_id: ? }, [userInfo.id])
+
+    // knex('social_profiles').insert()
+
     // to refresh token at some point, use
     // const newToken = await this.getNewAccessTokenUsingRefreshToken(token.refresh_token)
+
+    // create jwt and return (forward? redirect?)
 
     reply.header('x-access-blargy', token.access_token)
     reply.send({ message: 'Hello, Blargy' })
     // reply.redirect('http://localhost:8080/')
-
-    // check database for user; add if missing and store profile info, including access_token
-
-    // create jwt and return (forward? redirect?)
 
   })
 }
