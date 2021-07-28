446a07e61f595908efdacdd11b99ff59f047fc0e
Dave Mount <davemount@gmail.com>
2021-07-27 17:06:42 -0700

    Piecing things together, slowly.

--- db/identity/index.js
+++ db/identity/index.js
@@ -1,8 +1,35 @@
-export const findUser = async (fastify, providerPlatform, socialId) => {
+const findUser = async (fastify, providerPlatform, socialId) => {
   const { knex, log } = fastify
+
+  const platformId = await knex('system_codes')
+    .select('id')
+    .where({ public_id: providerPlatform })
+  if (!platformId) {
+    throw 'Unsupported social platform'
+  } else {
+    log.info(`rows: ${platformId.length}`)
+    log.info(`Social Platform ID: ${platformId[0].id}`)
+  }
+
+  const result = await knex('social_profiles')
+    .select('user_id')
+    .where({ social_id: socialId })
+    .andWhere('social_platform_id', '=', platformId[0].id)
+
+  log.info('got a result')
+  log.info(result)
+
+  if (result.length > 0) {
+    return result[0]
+  } else {
+    return null
+  }
   // const socialUserId = knex('social_profiles')
   //   .select('user_id')
   //   .join('system_codes', 'social_profiles.') // system_codes.public_id = 'github' and system_codes.id = social_profiles.social_platform_id
   //   .where({ social_id: ? }, [userInfo.id])
+}
 
+module.exports = {
+  findUser,
 }
--- routes/login/index.js
+++ routes/login/index.js
@@ -1,10 +1,14 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/test', async function (request, reply) {
+  fastify.get('/bloop', async function (request, reply) {
 
     const identity = require('../../db/identity')
     const user = await identity.findUser(fastify, 'github', 1234)
+    if (user) {
     reply.send(user.data)
+    } else {
+      reply.callNotFound()
+    }
   })
 }
