f16bc0edd4de7efea93ffe0c1ac6523271c4dbee
Dave Mount <davemount@gmail.com>
2021-07-28 18:09:46 -0700

    Refactor auth platform ID lookup

--- db/identity/index.js
+++ db/identity/index.js
@@ -1,71 +1,67 @@
+const getAuthProviderId = async (fastify, providerCode) => {
+  const { knex } = fastify
+  const platformRecord = await knex('system_codes')
+    .select('id')
+    .where({ public_id: providerCode })
+  if (platformRecord.length < 1) {
+    throw 'Unsupported social platform'
+  }
+  return platformRecord[0].id
+}
 
 /**
  * Looks for user based on auth provider info. Returns public ID.
  */
-const findUser = async (fastify, providerPlatform, socialId) => {
+const findUser = async (fastify, providerCode, socialId) => {
   const { knex, log } = fastify
 
-  const platformId = await knex('system_codes')
-    .select('id')
-    .where({ public_id: providerPlatform })
-  if (!platformId) {
-    throw 'Unsupported social platform'
-  } else {
-    log.info(`rows: ${platformId.length}`)
-    log.info(`Social Platform ID: ${platformId[0].id}`)
-  }
+  const authProviderId = await getAuthProviderId(fastify, providerCode)
 
-  let result = await knex('social_profiles')
+  let profileRecord = await knex('social_profiles')
     .select('user_id')
     .where({ social_id: socialId })
-    .andWhere('social_platform_id', '=', platformId[0].id)
+    .andWhere('social_platform_id', '=', authProviderId)
 
-  if (result.length < 1) {
+  if (profileRecord.length < 1) {
     return null
   }
 
-  return getUser(fastify, result[0].user_id)
+  return getUser(fastify, profileRecord[0].user_id)
 }
 
 const getUser = async (fastify, publicId) => {
-  const { knex, log } = fastify
-  const userRecord = await knex('users').select().where('public_id', '=', publicId)
+  const { knex } = fastify
+  const userRecord = await knex('users')
+    .select()
+    .where('public_id', '=', publicId)
   return userRecord.length > 0 ? userRecord[0] : null
 }
 
 // social profile includes: socialId, name, alias, email, avatarUrl
-const registerUser = async (fastify, providerPlatform, socialProfile) => {
-  const { knex, log } = fastify
+const registerUser = async (fastify, providerCode, accessToken, socialProfile) => {
+  const { knex } = fastify
 
-  // make sure auth provider is known
-  const platformRecord = await knex('system_codes')
-    .select('id')
-    .where({ public_id: providerPlatform })
-  if (platformRecord.length < 1) {
-    throw 'Unsupported social platform'
-  }
-  log.info(`rows: ${platformRecord.length}`)
-  log.info(`Social Platform ID: ${platformRecord[0].id}`)
-  const platformId = platformRecord[0].id
+  const authProviderId = await getAuthProviderId(fastify, providerCode)
 
   // create user record
-  const userRecord = await knex('users')
-    .insert({
-      'screen_name': socialProfile.name,
-      'email': socialProfile.email,
-      'avatar_url': socialProfile.avatar_url
+  const userRecord = await knex('users').insert(
+    {
+      screen_name: socialProfile.name,
+      email: socialProfile.email,
+      avatar_url: socialProfile.avatar_url,
     },
-    ['public_id']) // return value to use in next query
+    ['public_id']
+  ) // return value to use in next query
   const userPublicId = userRecord[0].public_id
 
   // create social record
-  const socialRecord = await knex('social_profiles')
-    .insert({
-      user_id: userRecord[0].public_id,
-      social_id: socialProfile.id,
-      social_platform_id: platformId,
-      social_user_info: socialProfile,
-    })
+  const socialRecord = await knex('social_profiles').insert({
+    user_id: userRecord[0].public_id,
+    social_id: socialProfile.id,
+    social_platform_id: authProviderId,
+    access_token: accessToken,
+    social_user_info: socialProfile,
+  })
 
   // return user with guest permissions
   return getUser(fastify, userPublicId)
--- routes/testing/index.js
+++ routes/testing/index.js
@@ -3,7 +3,6 @@
 const { findUser, getUser, registerUser } = require('../../db/identity')
 
 module.exports = async function (fastify, opts) {
-
   fastify.get('/findUser', async function (request, reply) {
     const user = await findUser(fastify, 'github', '12345')
     if (user) {
@@ -14,10 +13,7 @@ module.exports = async function (fastify, opts) {
   })
 
   fastify.get('/getUser', async function (request, reply) {
-    const user = await getUser(
-      fastify,
-      'c90249e3-9b4f-4dc3-8cc9-daa2ef0a806d'
-    )
+    const user = await getUser(fastify, 'c90249e3-9b4f-4dc3-8cc9-daa2ef0a806d')
     if (user) {
       reply.send(user)
     } else {
@@ -26,14 +22,19 @@ module.exports = async function (fastify, opts) {
   })
 
   fastify.post('/blarp', async function (request, reply) {
-    const user = await registerUser(fastify, 'github', {
-      id: 12345,
-      email: 'blargy@blargy.com',
-      name: 'Blargy2 Blargy',
-      avatar_url: 'http://avatarsrus.com/blargy2',
-      extra: 'wampas',
-      fun_fact: 'love exploring active underwater volcanos',
-    })
+    const user = await registerUser(
+      fastify,
+      'github',
+      'github-access_token_goes_here',
+      {
+        id: 12345,
+        email: 'badoongyface@blargy.com',
+        name: 'Badoongy Face',
+        avatar_url: 'http://avatarsrus.com/badoongy',
+        extra: 'abc 123',
+        fun_fact: 'love exploring dormant underwater volcanos caverns',
+      }
+    )
     if (user) {
       reply.send(user)
     } else {
