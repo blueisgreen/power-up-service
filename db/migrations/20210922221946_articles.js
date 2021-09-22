f77cf770bd08c45140ac78f7251de09b455e7ac0
Dave Mount <davemount@gmail.com>
2021-09-22 15:27:21 -0700

    Finish migrations for tables define so far

---
+++ db/migrations/20210922221946_articles.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('articles', (table) => {
+    table.increments('id')
+    table.string('headline')
+    table.string('byline')
+    table.string('cover_art_url')
+    table.text('content')
+    table.timestamps(true, true)
+    table.timestamp('published_at')
+    table.timestamp('archived_at')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('articles')
+}
