8b1896bbd6f0565c14733c8e514eb92e31de85b8
Dave Mount <davemount@gmail.com>
2021-09-26 16:34:57 -0700

    Get db connection from environment

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://powerup_admin:blue!$Green@localhost/powerup_db-test',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations',
     },
