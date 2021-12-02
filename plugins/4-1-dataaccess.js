488caf4ef16925ad4c213b078d90e97a20ec4764
Dave Mount <davemount@gmail.com>
2021-12-01 17:44:14 -0800

    think about data access plugin and testing

---
+++ plugins/4-1-dataaccess.js
@@ -0,0 +1,8 @@
+'use strict'
+const fp = require('fastify-plugin')
+const identity = require('../db/access/identity')
+
+module.exports = fp(async function (fastify, opts) {
+  // fastify.log.info('loading power up data access module')
+  // fastify.register()
+})
