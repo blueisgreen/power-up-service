3749e5384a7c739aaf2e7aae36c3ccaa47c31104
Dave Mount <davemount@gmail.com>
2021-12-14 22:23:22 -0800

    Rough out way to stub out auth (for now)

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,66 +1,48 @@
 'use strict'
 
-const GOTO_HOME = 'home'  // FIXME: extract method from github login to determine redirect target
-const validProviderIds = ['github']
+const GOTO_HOME = 'home'
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     const { log } = fastify
     const pid = request.query.pid
 
-    if (!request.anonymous) {
-      log.info(`sign in ${request.userKey}`)
-
-      // find user with given ID and identity provider
-      const user = await fastify.data.identity.findUserWithPublicId(
-        request.userKey,
-        pid
-      )
-      if (user !== null) {
-        log.debug(`found user ${JSON.stringify(user)}`)
-
-        // do i have a session token?
-        const token = await fastify.data.identity.findSessionToken(
-          user.userKey
-        )
-        if (token) {
-          log.debug(`found session token ${token}`)
-          const validToken = fastify.jwt.verify(token)
-          if (validToken) {
-            log.debug(`session token found: ${JSON.stringify(validToken)}`)
-            const who = validToken.who
-
-            reply.setCookie('who', who, fastify.cookieOptions)
-            reply.setCookie('token', token, fastify.cookieOptions)
-            reply.header('Authorization', `Bearer ${token}`)
-            reply.redirect(
-              `${process.env.SPA_LANDING_URL}?token=${token}&goTo=${GOTO_HOME}`
-            )
-            return
-          } else {
-            log.warn(`stored session token not valid: ${JSON.stringify(token)}`)
-          }
-        } else {
-          log.debug(`no session token found for ${request.userKey}`)
-        }
-      }
-    }
-
-    // if not handled, fall through to last resort
-    log.info('sign in unknown user')
-    const isSupportedProvider = validProviderIds.includes(pid)
-
-    if (isSupportedProvider) {
-      log.debug(`authenticate with provider: ${pid}`)
-      reply.clearCookie('who')
-      reply.redirect(`/login/${pid}`)
-      return
-    } else {
-      const errorMsg = 'unsupported identity provider: ' + pid
-      log.error(errorMsg)
-      reply.code(500)
-      reply.send(errorMsg)
+    if (request.anonymous || pid !== 'bypass') {
+      reply.code(401)
+      reply.send("I hear you knocking, but you can't come in.")
       return
     }
+
+    // look up user based on cookie
+    const user = await fastify.data.identity.findUserWithPublicId(
+      request.userKey,
+      pid
+    )
+
+    if (!user) {
+      reply.code(401)
+      reply.send('Who are you?')
+      return
+    }
+
+    log.debug(`found user ${JSON.stringify(user)}`)
+
+    // do i have a session token?
+    let token = await fastify.data.identity.findSessionToken(user.userKey)
+
+    // nope, create one
+    if (!token) {
+      token = fastify.jwt.sign({
+        user: {
+          who: user.userKey,
+          alias: user.alias,
+          roles,
+        },
+      })
+      await fastify.data.identity.setSessionToken(user.userKey, token)
+    }
+
+    reply.header('Authorization', `Bearer ${token}`)
+    reply.redirect(`${process.env.SPA_LANDING_URL}?goTo=home&token=${token}`)
   })
 }
