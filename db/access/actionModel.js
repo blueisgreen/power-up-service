c4cd362b2632fc14b2440fe8537e8cee03b81ee3
Dave Mount <davemount@gmail.com>
2022-04-05 18:28:31 -0700

    Clean up

--- db/access/actionModel.js
+++ db/access/actionModel.js
@@ -34,24 +34,6 @@ module.exports = (fastify) => {
 
   */
 
-  // function dateIsValid(dateStr) {
-  //   // format looks correct
-  //   const regex = /^\d{4}-\d{2}-\d{2}$/
-  //   if (dateStr.match(regex) === null) {
-  //     return false
-  //   }
-
-  //   // date is legitimate
-  //   const date = new Date(dateStr)
-  //   const timestamp = date.getTime()
-  //   if (typeof timestamp !== 'number' || Number.isNaN(timestamp)) {
-  //     return false
-  //   }
-
-  //   // converts back to given
-  //   return date.toISOString().startsWith(dateStr)
-  // }
-
   /**
    * Return all activities records that match given filters.
    * @param {*} on Date to restrict view or unrestricted if not passed
