c10a026b6f2595583adb6658f0e3ca9741fb32d1
Dave Mount <davemount@gmail.com>
2022-07-08 17:26:11 -0700

    There's a lot to like.

--- routes/login/google/index.js
+++ routes/login/google/index.js
@@ -46,7 +46,7 @@ module.exports = async function (fastify, opts) {
     fastify.data.action.capture(
       'login',
       request.tracker,
-      user.public_id,
+      user.userKey,
       browserContext
     )
 
@@ -54,12 +54,12 @@ module.exports = async function (fastify, opts) {
     const roles = await fastify.data.identity.getUserRoles(user.id)
     const token = fastify.jwt.sign({
       user: {
-        who: user.public_id,
+        who: user.userKey,
         alias: user.alias,
         roles,
       },
     })
-    await fastify.data.identity.setSessionToken(user.public_id, token)
+    await fastify.data.identity.setSessionToken(user.userKey, token)
     reply.setCookie('token', token, fastify.secretCookieOptions)
     reply.redirect(`${process.env.SPA_LANDING_URL}?goTo=${goTo}&token=${token}`)
   })
--- routes/login/linkedin/index.js
+++ routes/login/linkedin/index.js
@@ -51,20 +51,20 @@ module.exports = async function (fastify, opts) {
     fastify.data.action.capture(
       'login',
       request.tracker,
-      user.public_id,
-      browserContext,
+      user.userKey,
+      browserContext
     )
 
     // refresh token
     const roles = await fastify.data.identity.getUserRoles(user.id)
     const token = fastify.jwt.sign({
       user: {
-        who: user.public_id,
+        who: user.userKey,
         alias: user.alias,
         roles,
       },
     })
-    await fastify.data.identity.setSessionToken(user.public_id, token)
+    await fastify.data.identity.setSessionToken(user.userKey, token)
     reply.setCookie('token', token, fastify.secretCookieOptions)
     reply.redirect(`${process.env.SPA_LANDING_URL}?goTo=${goTo}&token=${token}`)
   })
